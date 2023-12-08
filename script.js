b2ff4f65f01d457a99d5d76a9a38c64e9fa580c0
asmita1000 <146213507+asmita1000@users.noreply.github.com>
2023-12-08 09:21:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// SIMPLE ADDITION IPO CALCULATOR
+
+// Input
+
+let number1 = +prompt("Enter a number:");
+let number2 = +prompt("Enter another number:");
+let number3 = +prompt("Enter another number:");
+
+// Process
+let total = number1 + number2 + number3;
+
+// Output
+alert(`${number1} + ${number2} +${number3} = ${total}`);
